13f712dc24d7347cc300ba9e561eac09f45cac6f
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2025-07-28 23:47:05 +1000

    Implement playlist

--- src/playlist.rs
+++ src/playlist.rs
@@ -1,1 +1,72 @@
-pub struct Playlist;
+use std::str::FromStr;
+
+use nom::{
+    Parser,
+    branch::alt,
+    character::complete::{char, line_ending, not_line_ending},
+    combinator::map,
+    error::Error,
+    multi::separated_list0,
+    sequence::preceded,
+};
+
+#[derive(Debug)]
+pub struct Playlist {
+    pub entries: Vec<String>,
+    pub comments: Vec<String>,
+}
+
+#[derive(Debug)]
+pub enum PlaylistParseError {}
+
+enum Line {
+    Comment(String),
+    Uri(String),
+    Empty,
+}
+
+impl FromStr for Playlist {
+    type Err = PlaylistParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines = separated_list0(
+            line_ending::<_, Error<&str>>,
+            alt((
+                preceded(
+                    char('#'),
+                    map(not_line_ending, |comment: &str| {
+                        Line::Comment(comment.to_string())
+                    }),
+                ),
+                map(not_line_ending, |s: &str| {
+                    if s.is_empty() {
+                        Line::Empty
+                    } else {
+                        Line::Uri(s.to_string())
+                    }
+                }),
+            )),
+        )
+        .parse(s)
+        .unwrap()
+        .1
+        .into_iter();
+
+        let mut entries = Vec::new();
+        let mut comments = Vec::new();
+
+        for line in lines {
+            match line {
+                Line::Comment(comment) => {
+                    comments.push(comment);
+                }
+                Line::Uri(uri) => {
+                    entries.push(uri);
+                }
+                Line::Empty => {}
+            }
+        }
+
+        Ok(Self { entries, comments })
+    }
+}
